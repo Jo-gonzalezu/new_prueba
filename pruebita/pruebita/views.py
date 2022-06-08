0e88561caa216024564b73c6148138e1a4436f2c
Ｊｏｒｇｅ　委-シ <90359419+Jo-gonzalezu@users.noreply.github.com>
2022-06-07 21:01:46 -0400

    backup
    prueba + django

---
+++ pruebita/pruebita/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from django.template import Template, Context 
+
+def inicio(request):
+    doc_externo = open("static/index.html")
+    plt = Template(doc_externo.read())
+    doc_externo.close()
+    ctx = Context()         #ejecuta python
+    documento = plt.render(ctx)
+    return HttpResponse(documento)      #devuelve html
+
