b04e47b3cbfc64f97a66a457c151c30e2af77137
Jo-gonzalezu <jorgegonzalezuribee@gmail.com>
2022-06-14 23:41:28 -0400

    paginas linkeadas

--- pruebita/pruebita/views.py
+++ pruebita/pruebita/views.py
@@ -9,3 +9,34 @@ def inicio(request):
     documento = plt.render(ctx)
     return HttpResponse(documento)      
 
+def seguimiento(request):
+    doc_externo = open("views\seguir.html") 
+    plt = Template(doc_externo.read())
+    doc_externo.close()
+    ctx = Context()       
+    documento = plt.render(ctx)
+    return HttpResponse(documento)     
+
+def registro(request):
+    doc_externo = open("static\views\register_new.html")
+    plt = Template(doc_externo.read())
+    doc_externo.close()
+    ctx = Context()       
+    documento = plt.render(ctx)
+    return HttpResponse(documento)         
+
+def fundacion(request):
+    doc_externo = open("static\views\donaciones.html")
+    plt = Template(doc_externo.read())
+    doc_externo.close()
+    ctx = Context()       
+    documento = plt.render(ctx)
+    return HttpResponse(documento)         
+
+def productos(request):
+    doc_externo = open("static\views\productos.html")
+    plt = Template(doc_externo.read())
+    doc_externo.close()
+    ctx = Context()       
+    documento = plt.render(ctx)
+    return HttpResponse(documento)         
