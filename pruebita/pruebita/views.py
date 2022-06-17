4ebb4d5975c1d2883691253844538f9af581e8c8
Jo-gonzalezu <jorgegonzalezuribee@gmail.com>
2022-06-16 21:09:29 -0400

    avance_clase

--- pruebita/pruebita/views.py
+++ pruebita/pruebita/views.py
@@ -1,5 +1,16 @@
 from django.http import HttpResponse
 from django.template import Template, Context 
+from django.shortcuts import render 
+from productos.models import Productos
+
+def login(request):
+    return render(request,"views_html/register_new.html")
+
+# para acceder a los parametros get se debe utilizar la siguiente sintaxis
+def resultado(request):
+    #mensaje = f'se ha logeado el usuario {request.GET["correo"]}'
+    llamadabd = Productos.objects.filter(nombre_icontains="plantita_weed")
+    return render(request,"static/views_html/resulado.html",{'mensaje':mensaje})
 
 def inicio(request):
     doc_externo = open("static/index.html")
@@ -10,7 +21,7 @@ def inicio(request):
     return HttpResponse(documento)      
 
 def seguimiento(request):
-    doc_externo = open("views\seguir.html") 
+    doc_externo = open("static/views_html/seguir.html") 
     plt = Template(doc_externo.read())
     doc_externo.close()
     ctx = Context()       
@@ -18,7 +29,7 @@ def seguimiento(request):
     return HttpResponse(documento)     
 
 def registro(request):
-    doc_externo = open("static\views\register_new.html")
+    doc_externo = open("static/views_html/register_new.html")
     plt = Template(doc_externo.read())
     doc_externo.close()
     ctx = Context()       
@@ -26,7 +37,7 @@ def registro(request):
     return HttpResponse(documento)         
 
 def fundacion(request):
-    doc_externo = open("static\views\donaciones.html")
+    doc_externo = open("static/views_html/donaciones.html")
     plt = Template(doc_externo.read())
     doc_externo.close()
     ctx = Context()       
@@ -34,7 +45,7 @@ def fundacion(request):
     return HttpResponse(documento)         
 
 def productos(request):
-    doc_externo = open("static\views\productos.html")
+    doc_externo = open("static/views_html/productos.html")
     plt = Template(doc_externo.read())
     doc_externo.close()
     ctx = Context()       
