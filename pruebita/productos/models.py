409cbd342d3a2f3b38451d21f7ef8cd8f296ffab
kenny castillo <kenny.15.jax@gmail.com>
2022-06-23 00:26:24 -0400

    Agregado de tablas

--- pruebita/productos/models.py
+++ pruebita/productos/models.py
@@ -8,3 +8,17 @@ class Productos(models.Model):
     disponible = models.BooleanField()
     fechaIncorporacion = models.DateField()
     correoProveedor = models.EmailField()
+
+class cliente(models.Model):
+    rut = models.IntegerField()
+    nombre = models.CharField(max_length=30)
+    apellidoPa = models.CharField(max_length=30)
+    apellidoMa = models.CharField(max_length=30)
+    nacimiento = models.DateField()
+    correo = models.EmailField()
+
+class boleta(models.Model):
+    nroBoleta = models.IntegerField()
+    cantidadProductos = models.IntegerField()
+    total = models.FloatField()
+    fecha = models.DateField()
